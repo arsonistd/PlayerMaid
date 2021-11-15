f5bd48fd911a2e8002b1f1241b79a5c5d87b99dd
ArsonistD <hotdadog@gmail.com>
2021-11-15 05:58:57 -0500

    added syntax and player added callback

--- src/playermaid/init.lua
+++ src/playermaid/init.lua
@@ -1,3 +1,4 @@
+
 -- PlayerMaid.lua
 -- ArsonistD
 -- Started : 11/14/2021
@@ -20,20 +21,57 @@ PlayerMaid.__index = PlayerMaid
     @function new
     @within PlayerMaid
 ]=]
-function PlayerMaid.new()
-    local self = setmetatable({}, PlayerMaid)
+function PlayerMaid.new(syntax)
+	local self = setmetatable({}, PlayerMaid)
 
-    self._players = {}
+	self._players = {}
+	
+	for _, player in pairs(Players:GetPlayers()) do
+		self._players[player.UserId] = {}
+	end
 
-    self._playerAddedConnection = Players.PlayerAdded:Connect(function(player)
-        self._players[player.UserId] = {}
-    end)
-    self._playerRemovingConnection = Players.PlayerRemoving:Connect(function(player)
-        self:CleanPlayer(player.UserId)
-        self._players[player.UserId] = nil
-    end)
+	self._playerAddedConnection = Players.PlayerAdded:Connect(function(player)
+		if self.playerAddedCallback then
+			self.playerAddedCallback(player)
+		end
+		self._players[player.UserId] = {}
+		print(self._players)
+	end)
+	
+	self._playerRemovingConnection = Players.PlayerRemoving:Connect(function(player)
+		self:CleanPlayer(player.UserId)
+		self._players[player.UserId] = nil
+	end)
 
-    return self
+	if syntax then
+		if string.lower(syntax) == "maid" then
+			self.GiveTask = self.Add
+			self.DoCleaning = self.Clean
+			self.DoCleaningPlayer = self.CleanPlayer
+		elseif string.lower(syntax) == "janitor" then
+			self.Cleanup = self.Clean
+			self.CleanupPlayer = self.CleanPlayer
+		elseif string.lower(syntax) == "dumpster" then
+			self.dump = self.Add
+			self.burn = self.Clean
+			self.burnPlayer = self.CleanPlayer
+		elseif string.lower(syntax) == "trove" then
+			-- syntax is already like trove ¯\_(ツ)_/¯
+		end 
+	end
+
+	return self
+end
+
+--[=[
+    Sets a callback function that will be fired when a player joins
+
+    @function setPlayerAddedCallback
+    @within PlayerMaid
+    @param callback any -- The callback function that you want to fire
+]=]
+function PlayerMaid:setPlayerAddedCallback(callback)
+    self.playerAddedCallback = callback
 end
 
 
@@ -46,12 +84,15 @@ end
     @param object any -- The object that will be cleaned
 ]=]
 function PlayerMaid:Add(player: any, obj: any)
-    if obj then
-        local index = #self._players[player.UserId]+1
-        self._players[player.UserId][index] = obj
-    else
-        error("Object does not exist")
-    end
+	assert(player ~= nil, "Argument 1 is missing or nil")
+	assert(obj ~= nil, "Argument 2 is missing or nil")
+
+	if obj then
+		print(self._players)
+		local playerContainer = self._players[player.UserId]
+		local index = #playerContainer+1
+		self._players[player.UserId][index] = obj
+	end
 end
 
 
@@ -62,10 +103,10 @@ end
     @within PlayerMaid
 ]=]
 function PlayerMaid:Clean()
-    -- Cleanup every player
-    for userId, playerObjs in pairs(self._players) do
-        self:CleanPlayer(userId)
-    end
+	-- Cleanup every player
+	for userId, playerObjs in pairs(self._players) do
+		self:CleanPlayer(userId)
+	end
 end
 
 
@@ -77,34 +118,34 @@ end
     @param userId number -- The player's UserId that you want to clean.
 ]=]
 function PlayerMaid:CleanPlayer(userId)
-    local playerObjs = self._players[userId]
+	local playerObjs = self._players[userId]
 
 
-    -- Clean connections
-    for _, obj in pairs(playerObjs) do
-        if typeof(obj) == "RBXScriptConnection" then
-            obj:Disconnect()
-            playerObjs[_] = nil
-        end
-    end
+	-- Clean connections
+	for _, obj in pairs(playerObjs) do
+		if typeof(obj) == "RBXScriptConnection" then
+			obj:Disconnect()
+			playerObjs[_] = nil
+		end
+	end
 
-    -- Fire functions that are indexed
-    for _, obj in pairs(playerObjs) do
-        if type(obj) == "function" then -- If object is a function then call it
-            obj()
-            playerObjs[_] = nil
-        end
-    end
+	-- Fire functions that are indexed
+	for _, obj in pairs(playerObjs) do
+		if type(obj) == "function" then -- If object is a function then call it
+			obj()
+			playerObjs[_] = nil
+		end
+	end
 
-    -- Destroy objs that are indexed
-    for _, obj in pairs(playerObjs) do
-        if obj.Destroy then -- If obj has a destroy method then Destroy()
-            task:Destroy()
-            playerObjs[_] = nil
-        end
-    end
+	-- Destroy objs that are indexed
+	for _, obj in pairs(playerObjs) do
+		if obj.Destroy then -- If obj has a destroy method then Destroy()
+			task:Destroy()
+			playerObjs[_] = nil
+		end
+	end
 
-    self._players[userId] = {}
+	self._players[userId] = {}
 end
 
 --[=[
@@ -114,12 +155,12 @@ end
     @within PlayerMaid
 ]=]
 function PlayerMaid:Destroy()
-    self._playerAddedConnection:Disconnect()
-    self._playerRemovingConnection:Disconnect()
+	self._playerAddedConnection:Disconnect()
+	self._playerRemovingConnection:Disconnect()
 
-    self:Clean()
+	self:Clean()
 
-    self._players = {}
+	self._players = {}
 end
 
 return PlayerMaid
